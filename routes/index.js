876ce1e7148f0e08a43b648f18e145596d3cf97d
Edwinaeded <edwinaeded@gmail.com>
2024-12-02 15:51:07 +0800

    Add auth-handler middlewares by using passport

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,14 @@ const router = express.Router()
 const root = require('./root')
 const restaurants = require('./restaurants')
 const users = require('./users')
+const authHandler = require('../middlewares/auth-handler')
 
 router.get('/', (req, res) => {
   res.send('hello world')
 })
 
 router.use('/', root)
-router.use('/restaurants', restaurants)
+router.use('/restaurants', authHandler, restaurants)
 router.use('/users', users)
 
 module.exports = router
